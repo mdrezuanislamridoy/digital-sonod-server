06cdfd8833a854e0088ebf67ba844b8fead5330d
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-08-30 16:35:19 +0600

    uploaded server code

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(halmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://digital-sonod.netlify.app",
     credentials: true,
   })
 );
