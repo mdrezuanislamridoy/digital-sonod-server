b34147ccc534b92e6f385123f8cd0fc511f17e6b
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-08-31 19:55:23 +0600

    updated sonod submission

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(halmet());
 app.use(
   cors({
-    origin: "http://localhost:5173", //"https://digital-sonod.netlify.app",
+    origin: "https://digital-sonod.netlify.app", //"https://digital-sonod.netlify.app","http://localhost:5173"
     credentials: true,
   })
 );
